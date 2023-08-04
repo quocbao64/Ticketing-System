00cfc40189fc61755eeb0992a2f3406d251fd6d8
quocbao64 <quocbao642002@gmail.com>
2023-08-04 15:50:35 +0700

    add kubernetes deployment and service

---
+++ kube-apply.sh
@@ -0,0 +1,11 @@
+kubectl apply -f ./api-gateway/src/main/resources/kubernetes
+
+kubectl apply -f ./car-service/src/main/resources/kubernetes
+
+kubectl apply -f ./discovery-server/src/main/resources/kubernetes
+
+kubectl apply -f ./email-service/src/main/resources/kubernetes
+
+kubectl apply -f ./order-service/src/main/resources/kubernetes
+
+kubectl apply -f ./payment-service/src/main/resources/kubernetes
